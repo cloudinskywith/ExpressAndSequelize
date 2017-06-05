d72a2ad500bbc1f6d0b4ee757a4880105416adba
liaobaocheng <liaobaocheng90@gmail.com>
2017-06-05 18:22:49 +0800

    crud express

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,28 @@ var models = require('../models');
 router.get('/', function(req, res, next) {
     models.Task.findAll({attributes:['id','title','done']}).then(tasks=>{
         res.json({tasks:tasks});
+    }).catch(error=>{
+        res.status(412).json({msg:error.message});
     });
 });
+
+router.post('/',function (req,res,next) {
+    models.Task.create(req.body).then(result=>res.json(result)).catch(error=>{
+        res.status(412).json({msg:error.message});
+    });
+});
+
+router.get('/:id',function (req, res, next) {
+    models.Task.findOne({where:req.params,attributes:['id']}).then(result=>{
+        if(result){
+            res.json(result);
+        }else{
+            res.sendStatus(404);
+        }
+    }).catch(error=>{
+        res.status(412).json({msg:error.message});
+    })
+});
+
 
 module.exports = router;
